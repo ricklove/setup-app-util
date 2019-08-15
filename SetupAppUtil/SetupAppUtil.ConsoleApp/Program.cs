775a29db679cbd914f91f64c63e1a05b73036ba1
Rick Love <rick@toldpro.com>
2019-08-15 16:01:52 -0500

    Improve Setup Process for Updating

--- SetupAppUtil/SetupAppUtil.ConsoleApp/Program.cs
+++ SetupAppUtil/SetupAppUtil.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 
 namespace SetupAppUtil.ConsoleApp
@@ -7,11 +8,33 @@ namespace SetupAppUtil.ConsoleApp
     {
         private static void Main(string[] args)
         {
+            var attempts = 0;
+            while (attempts <= 3)
+            {
+                Console.WriteLine($"Installing...{(attempts > 0 ? $"attempt {attempts + 1}" : "")}");
+
+                try
+                {
+                    System.Threading.Thread.Sleep(1000 * attempts * attempts);
+                    RunSetup();
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}");
+                }
+
+                attempts++;
+            }
+        }
+
+        private static void RunSetup()
+        {
             // Testing
             // Directory.SetCurrentDirectory(@"\\smiths.net\dfs\Work\Flex-Tek\STUT\Installs\TutcoTools\PdfMark");
             var destRootDir = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData), "Apps");
 
-            var configFilePath = "setup.cfg";
+            var configFilePath = "setup.config";
             if (File.Exists(configFilePath))
             {
                 var configDoc = File.ReadAllText(configFilePath);
@@ -20,6 +43,8 @@ namespace SetupAppUtil.ConsoleApp
             }
 
             // Copy Files
+            Console.WriteLine("Copying Files...");
+
             var dir = Directory.GetDirectories(Directory.GetCurrentDirectory()).First();
             var destDir = Path.Combine(destRootDir, new DirectoryInfo(dir).Name);
             var files = Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories);
@@ -33,9 +58,15 @@ namespace SetupAppUtil.ConsoleApp
                 File.Copy(file, destFile, true);
             }
 
+            File.WriteAllText(Path.Combine(destDir, "setup.config"), dir);
+
             // Start Exe
+            Console.WriteLine("Running...");
             var appExe = Directory.GetFiles(destDir, "*.exe").First();
             System.Diagnostics.Process.Start(appExe);
+
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine("Done...");
         }
     }
 }
